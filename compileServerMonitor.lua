7cd260376fc2577b58f5803da96e100eeb85d9e5
Rikenbacker <Rikenbacker@yandex.ru>
2022-10-30 22:46:29 +0300

    Create compileServerMonitor.lua

---
+++ compileServerMonitor.lua
@@ -0,0 +1,15 @@
+shell = require("shell")
+
+shell.execute("rm monitor.lua")
+shell.execute("rm ControllerConfig.lua")
+shell.execute("rm ControllerData.lua")
+shell.execute("rm List_C.lua")
+shell.execute("rm add_controller.lua")
+
+shell.execute("pastebin get VJsyG6ER monitor.lua")
+shell.execute("pastebin get cYcsRv6g ControllerConfig.lua")
+shell.execute("pastebin get N5AQ1kvr ControllerData.lua")
+shell.execute("pastebin get UAQ6KVEs List_C.lua")
+shell.execute("pastebin get U0rmaEeH add_controller.lua")
+
+shell.execute("monitor")
