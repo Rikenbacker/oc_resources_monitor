31df5e0fdabcd595568f67b0f4f1932839ad701a
Rikenbacker <Rikenbacker@yandex.ru>
2022-10-30 22:40:44 +0300

    Create ServersConfig.lua

---
+++ ServersConfig.lua
@@ -0,0 +1,27 @@
+ServersConfig = {}
+ 
+function ServersConfig:new(address)
+    -- свойства
+    local obj= {}
+        obj.address = address
+ 
+    --чистая магия!
+    setmetatable(obj, self)
+    self.__index = self; return obj
+end
+ 
+function readServersConfig()
+   local f=io.open(serversFile,"r")
+   if f~=nil then 
+		io.close(f) 
+    else
+		return nil
+	end
+
+    local serversConfigArray = {}
+    for line in io.lines(serversFile) do
+        serversConfigArray[#serversConfigArray + 1] = ServersConfig:new(line)
+    end
+   
+    return serversConfigArray
+end
