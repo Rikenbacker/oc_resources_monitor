3aba0234f51809093ef66ff067972f3743a64375
Rikenbacker <Rikenbacker@yandex.ru>
2022-11-09 08:30:35 +0300

    Update ServersData.lua

--- ServersData.lua
+++ ServersData.lua
@@ -19,3 +19,26 @@ function scanRemoteServers(controllers, srvCfg)
 
     return controllers
 end
+
+function scanRemoteBattServers(batBuffers, srvBattCfg)
+    if #srvBattCfg == 0 then
+        return batBuffers
+    end
+
+    local receivedData = ""
+    for i = 1, #srvBattCfg do
+        modem.send(srvBattCfg[i].address, listenedPort, "data")
+        local _, _, _, _, _, message = event.pull(1, "modem_message")
+
+        if message ~= nil then
+            receivedData = receivedData .. message
+        end
+    end
+
+    local additional = unserializeBattareis(receivedData)
+    for i = 1, #additional do
+        batBuffers[#batBuffers + 1] = additional[i]
+    end
+
+    return batBuffers
+end
