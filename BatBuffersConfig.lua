7b0f57009d8a29fd9c371cd08aa6c48b4ccd042b
Rikenbacker <Rikenbacker@yandex.ru>
2022-11-09 08:31:11 +0300

    Update BatBuffersConfig.lua

--- BatBuffersConfig.lua
+++ BatBuffersConfig.lua
@@ -12,6 +12,14 @@ end
  
 function readBatBuffersConfig()
     local batBuffersConfigArray = {}
+
+   local f=io.open(batBuffersFile,"r")
+   if f~=nil then 
+		io.close(f) 
+    else
+		return batBuffersConfigArray
+	end	
+	
     for line in io.lines(batBuffersFile) do
         batBuffersConfigArray[#batBuffersConfigArray + 1] = BatBuffersConfig:new(line)
     end
