1518ecde27731d8ed5eac413f1ca41b12d25afcd
Rikenbacker <Rikenbacker@yandex.ru>
2022-11-09 08:29:57 +0300

    Update ServersConfig.lua

--- ServersConfig.lua
+++ ServersConfig.lua
@@ -3,11 +3,12 @@ ServersConfig = {}
 function ServersConfig:new(address)
     -- свойства
     local obj= {}
-        obj.address = address
+    obj.address = address
  
     --чистая магия!
     setmetatable(obj, self)
-    self.__index = self; return obj
+    self.__index = self; 
+	return obj
 end
  
 function readServersConfig()
@@ -20,6 +21,22 @@ function readServersConfig()
 
     local serversConfigArray = {}
     for line in io.lines(serversFile) do
+        serversConfigArray[#serversConfigArray + 1] = ServersConfig:new(line)
+    end
+   
+    return serversConfigArray
+end
+
+function readServersBattConfig()
+   local f=io.open(serversBattFile,"r")
+   if f~=nil then 
+		io.close(f) 
+    else
+		return nil
+	end
+
+    local serversConfigArray = {}
+    for line in io.lines(serversBattFile) do
         serversConfigArray[#serversConfigArray + 1] = ServersConfig:new(line)
     end
    
